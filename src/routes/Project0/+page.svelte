07c761e5bc47d3b29cf25a1c37c0b6d28779f0d0
j0sh05 <97497822+j0sh05@users.noreply.github.com>
2023-01-12 23:46:47 -1000

    Finish Project 0

--- src/routes/Project0/+page.svelte
+++ src/routes/Project0/+page.svelte
@@ -2,16 +2,25 @@
     const fall = '/img/fall.jpg';
     const mountain = '/img/mountains.jpg';
     const ocean = '/img/ocean.jpg';
-    const room = '/img/room.jpg'
+    const room = '/img/room.jpg';
+    let variable = room;
 
-    function customWallpaper() {
-        return ocean;
-    }
+    // function customWallpaper() {
+    //     return room;
+    // }
 
     // TODO 
     // Make images rotate based on the button that is clicked
 </script>
 
-<div class="w-full h-screen bg-center bg-no-repeat bg-cover" style="background-image: url({customWallpaper()});">
-    
+<div class="flex justify-center items-center w-full h-screen bg-center bg-no-repeat bg-cover" style="background-image: url({variable});">
+    <div class="w-1/4 h-1/2 bg-slate-900 opacity-70 shadow-2xl shadow-white rounded-3xl flex flex-col justify-center items-center gap-4">
+        <h4 class="text-white font-serif ">Press a button to change the background</h4>
+        <div class="text-white grid grid-cols-2 grid-rows-2 gap-4">
+            <button on:click={() => variable = mountain} class="bg-white text-black font-serif h-12 w-100 px-3 opacity-50 hover:opacity-70 active:opacity-100 rounded-2xl text-lg">Mountain</button>
+            <button on:click={() => variable = fall} class="bg-white text-black font-serif h-12 w-100 px-3 opacity-50 hover:opacity-70 active:opacity-100 rounded-2xl text-lg">Fall</button>
+            <button on:click={() => variable = room} class="bg-white text-black font-serif h-12 w-100 px-3 opacity-50 hover:opacity-70 active:opacity-100 rounded-2xl text-lg">Room</button>
+            <button on:click={() => variable = ocean} class="bg-white text-black font-serif h-12 w-100 px-3 opacity-50 hover:opacity-70 active:opacity-100 rounded-2xl text-lg">Ocean</button>
+        </div>
+    </div>
 </div>
