40515e8007721a7dd823e8dc25d6250196d1c315
j0sh05 <97497822+j0sh05@users.noreply.github.com>
2023-01-12 22:22:43 -1000

    Functioning Background

--- src/routes/Project0/+page.svelte
+++ src/routes/Project0/+page.svelte
@@ -4,9 +4,14 @@
     const ocean = '/img/ocean.jpg';
     const room = '/img/room.jpg'
 
-    
+    function customWallpaper() {
+        return ocean;
+    }
 
     // TODO 
     // Make images rotate based on the button that is clicked
 </script>
-<p>hello world</p>
+
+<div class="w-full h-screen bg-center bg-no-repeat bg-cover" style="background-image: url({customWallpaper()});">
+    
+</div>
