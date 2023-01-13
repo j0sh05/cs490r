2c1a62b434e250501a48a3e349340b2a1e474497
j0sh05 <97497822+j0sh05@users.noreply.github.com>
2023-01-13 08:41:20 -1000

    Minor changes to the button box. Need to make it mobile friendly

--- src/routes/Project0/+page.svelte
+++ src/routes/Project0/+page.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <div class="flex justify-center items-center w-full h-screen bg-center bg-no-repeat bg-cover" style="background-image: url({variable});">
-    <div class="w-1/4 h-1/2 bg-slate-900 opacity-70 shadow-2xl shadow-white rounded-3xl flex flex-col justify-center items-center gap-4">
+    <div class="w-1/4 h-1/3 bg-slate-900 opacity-70 shadow-2xl shadow-white rounded-3xl flex flex-col justify-center items-center gap-4">
         <h4 class="text-white font-serif flex text-center">Press a button to change the background</h4>
         <div class="text-white grid grid-cols-2 grid-rows-2 gap-4">
             <button on:click={() => variable = mountain} class="bg-white text-black font-serif h-12 w-100 px-3 opacity-50 hover:opacity-70 active:opacity-100 rounded-2xl text-lg">Mountain</button>
