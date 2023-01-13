90271d67692d3f60ab579c2684be1e5e0b5a3655
j0sh05 <97497822+j0sh05@users.noreply.github.com>
2023-01-12 23:55:40 -1000

    Make changes to README.md

--- src/routes/Project0/+page.svelte
+++ src/routes/Project0/+page.svelte
@@ -5,17 +5,11 @@
     const room = '/img/room.jpg';
     let variable = room;
 
-    // function customWallpaper() {
-    //     return room;
-    // }
-
-    // TODO 
-    // Make images rotate based on the button that is clicked
 </script>
 
 <div class="flex justify-center items-center w-full h-screen bg-center bg-no-repeat bg-cover" style="background-image: url({variable});">
     <div class="w-1/4 h-1/2 bg-slate-900 opacity-70 shadow-2xl shadow-white rounded-3xl flex flex-col justify-center items-center gap-4">
-        <h4 class="text-white font-serif ">Press a button to change the background</h4>
+        <h4 class="text-white font-serif flex text-center">Press a button to change the background</h4>
         <div class="text-white grid grid-cols-2 grid-rows-2 gap-4">
             <button on:click={() => variable = mountain} class="bg-white text-black font-serif h-12 w-100 px-3 opacity-50 hover:opacity-70 active:opacity-100 rounded-2xl text-lg">Mountain</button>
             <button on:click={() => variable = fall} class="bg-white text-black font-serif h-12 w-100 px-3 opacity-50 hover:opacity-70 active:opacity-100 rounded-2xl text-lg">Fall</button>
